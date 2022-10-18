33cdbbe0e7085f92b7f4bc02bb01fcb799aa47c0
swenw <swenwa@163.com>
2022-10-18 18:24:52 +0800

    Initial commit

---
+++ src/main/java/com/imooc/reader/service/TestService.java
@@ -0,0 +1,19 @@
+package com.imooc.reader.service;
+
+import com.imooc.reader.mapper.TestMapper;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+
+@Service
+public class TestService {
+    @Resource
+    private TestMapper testMapper;
+    @Transactional
+    public void batchImport() {
+        for (int i=0; i<2; i++) {
+            testMapper.insert();
+        }
+    }
+}
